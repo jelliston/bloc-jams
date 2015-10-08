f725612f686fb601bb685ef97c2e618f650177c3
jelliston <jelliston@gmail.com>
2015-10-08 09:54:51 +0300

    Checkpoint 23 completed, scripting events

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,15 +1,35 @@
 var points = document.getElementsByClassName('point');
 
-function revealPoints(i) {
+var animatePoints = function (points) {
+
+    var revealPoints = function(i) {
         points[i].style.transform = "scaleX(1) translateY(0)";
         points[i].style.msTransform = "scaleX(1) translateY(0)";
         points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
         points[i].style.opacity = 1;
     }
 
-document.addEventListener("DOMContentLoaded", function(){ 
-for (var i = 0; i < points.length; i++) {
-    revealPoints(i);
-}
-});
+    for (var i = 0; i < points.length; i++) {
+            revealPoints(i);
+        }
+};
 
+window.onload = function() {
+
+    // Automatically animate the points on a tall screen where scrolling can't trigger the animation
+     if (window.innerHeight > 950) {
+         animatePoints(points);
+     }
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    window.addEventListener('scroll',function(event) {
+        
+        if (document.body.scrollTop >= scrollDistance) {
+            animatePoints(points);
+        }
+        
+    });
+
+}
